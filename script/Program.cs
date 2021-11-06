58d7a2df620d62bab5afbe97ed68649cf3718f08
DenisRumyantsev <mr.denis.rumyantsev@gmail.com>
2021-11-06 16:18:29 +0300

    namespace research

--- script/Program.cs
+++ script/Program.cs
@@ -1,4 +1,8 @@
 ﻿
+using Research;
+
+DelegateResearch.Research();
+
 // =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====
 
 // class Abc
@@ -192,64 +196,5 @@
 //         private static void RedMsg(string message) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(message); Console.ResetColor(); }
 //     }
 // }
-
-// =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====
-
-using System;
-
-namespace script
-{
-    public delegate void Del();
-    public delegate void DelNum(int num);
-
-    class Program
-    {
-        public void Caller_1_() { Console.WriteLine("This is Caller 1 function..."); }
-        public static void Caller_2_() { Console.WriteLine("This is Caller 2 function..."); }
-
-        static void Main()
-        {
-            // Program.Main();
-
-            Program p = new Program();
-
-            List<Del> dlgt = new List<Del>()
-            {
-                new Del(p.Caller_1_),
-                new Del(Caller_2_),
-                new Del(
-                    delegate { Console.WriteLine("This is Caller 3 function..."); }
-                ),
-                new Del(
-                    () => { Console.WriteLine("This is Caller 4 function..."); }
-                )
-            };
-
-            List<DelNum> dlgtNum = new List<DelNum>()
-            {
-                new DelNum(
-                    delegate(int num) { Console.WriteLine($"This is Caller 5 function with {num} argument..."); }
-                ),
-                new DelNum(
-                    (int num) => { Console.WriteLine($"This is Caller 6 function with {num} argument..."); }
-                )
-            };
-
-            dlgt[0]();
-            dlgt[1]();
-            dlgt[2]();
-            dlgt[3]();
-
-            dlgtNum[0](5);
-            dlgtNum[1](6);
-
-            Del MultiDelegate0123 = dlgt[0] + dlgt[1] + dlgt[2] + dlgt[3];
-            MultiDelegate0123();
-
-            DelNum MultiDelegate56 = dlgtNum[0] + dlgtNum[1]; // Multi Cast Idea
-            MultiDelegate56(7);
-        }
-    }
-}
 
 // =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====   =====
---
+++ script/Researches/Delegate.Research.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Research
+{
+    public delegate void Del();
+    public delegate void DelNum(int num);
+
+    public class DelegateResearch
+    {
+        public void Caller_1_() { Console.WriteLine("This is Caller 1 function..."); }
+        public static void Caller_2_() { Console.WriteLine("This is Caller 2 function..."); }
+
+        public static void Research()
+        {
+            var DR = new DelegateResearch();
+
+            List<Del> dlgt = new List<Del>()
+            {
+                new Del(DR.Caller_1_),
+                new Del(Caller_2_),
+                new Del(
+                    delegate { Console.WriteLine("This is Caller 3 function..."); }
+                ),
+                new Del(
+                    () => { Console.WriteLine("This is Caller 4 function..."); }
+                )
+            };
+
+            List<DelNum> dlgtNum = new List<DelNum>()
+            {
+                new DelNum(
+                    delegate(int num) { Console.WriteLine($"This is Caller 5 function with {num} argument..."); }
+                ),
+                new DelNum(
+                    (int num) => { Console.WriteLine($"This is Caller 6 function with {num} argument..."); }
+                )
+            };
+
+            dlgt[0]();
+            dlgt[1]();
+            dlgt[2]();
+            dlgt[3]();
+
+            dlgtNum[0](5);
+            dlgtNum[1](6);
+
+            Del MultiDelegate0123 = dlgt[0] + dlgt[1] + dlgt[2] + dlgt[3];
+            MultiDelegate0123();
+
+            DelNum MultiDelegate56 = dlgtNum[0] + dlgtNum[1]; // Multi Cast Idea
+            MultiDelegate56(7);
+        }
+    }
+}
